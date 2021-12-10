b3bf856435a900d43fc0eee40d1be380ce81d648
Vurad-0x3D <89680795+Vurad-0x3D@users.noreply.github.com>
2021-12-10 20:18:12 +0300

    Add files via upload

--- config/playlist.py
+++ config/playlist.py
@@ -1,1 +1,5 @@
-playlis = '1. Lordi - Hard Rock Hallelujah\n2. BAV - ПЧЕЛОБАВ УРОД\n3. Ебанько - Ебу собак\n4. KSLV Noh - Disaster musmore\n5. Freddie Dredd - GTG\n6. PlayaPhonk - Phonky Town\n7. Liue - Suffer With Me Tiktok Edit\n8. Brand New - Not The Sun'
+playlis = '1. Lordi - Hard Rock Hallelujah\n2. BAV - ПЧЕЛОБАВ УРОД\n3. Ебанько - Ебу собак\n4. KSLV Noh - Disaster musmore\n5. Freddie Dredd - GTG\n6. PlayaPhonk - Phonky Town\n7. Liue - Suffer With Me Tiktok Edit\n8. Brand New - Not The Sun\n9. Brand New - Bed\
+10. Godsmack - Bulletproof\n11. Нервы - Твоим морем\n12. Нервы - Нервы\n13. DJ Ебан - Ебан\n14. Ебанько - Еби меня bass boosted\n15. RXLZQ - Town\n16. RXLZQ - Falling\n17. RXLZQ - FVCK\
+18. SYML - Mr.Sandman\n19. Автоспорт - Песня о привязанности\n20. Форсаж 5 – Мы едем в лето\n21. AXIUS LINK – не догонят\n22. My Chemical_Romance - Na Na Na Na Na Na Na Na Na Na Na Na\
+23. The Pretty Reckless - Just Tonight-1\n24. Творожное Озеро - Юные\n25. Molchat Doma - Тоска\26. Колбасный цех 6 – Track 16\n27. Skrillex & Damian - Make It Bun Dem\n28. Good buy - Mother Fucker\
+29. Julie Roué - Revolution Day'
